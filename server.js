1204e91dba3251d7bb75d47bb5279454d11cca5d
a-taghva <taghvali99@gmail.com>
2022-11-10 22:50:32 -0500

    db connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`server is running on PORT ${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`);
+  })
 })
